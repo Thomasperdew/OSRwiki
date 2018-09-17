9a1b4bf2c1951b46be78c3f0604b081893bfecb2
thomas perdew <thomasprdw@aol.com>
2018-09-17 15:44:31 -0600

    Updated php

--- Stats/Strength.php
+++ Stats/Strength.php
@@ -8,28 +8,11 @@
     <h1>OSRwiki</h1>
     <div class="grid-container">
         <div class="item1"><a href="/index.php">Home</a></div>
-        <div class="item2">
-            <div class="collapsible">Creating a Character</div>
-            <div class="content">
-                <li class="collapsible">Abilities</li>
-                <ul class="content">
-                    <li><a href="Strength.php">Strength</a></li>
-                    <li><a href="Dexterity.html">Dexterity</a></li>
-                    <li><a href="Constitution.html">Constitution</a></li>
-                    <li><a href="Intelligence.html">Intelligence</a></li>
-                    <li><a href="Wisdom.html">Wisdom</a></li>
-                    <li><a href="Charisma.html">Charisma</a></li>
-                </ul>
-                <li>Races</li>
-                <li>Classes</li>
-                <li>Alignment</li>
-                <li>Money</li>
-                <li>Equipment</li>
-                <li>Height and Weight</li>
-            </div>
+        <div>
+		        <?php include "menu.php"; ?>
         </div>
         <div class="item3">Strength</div>
-        <?php include "menu.php"; ?>
+
         <div class="item5">Footer</div>
     </div>
 
