fe364ba5cd00d065af3ed1cc2175f08be43c40a4
thomas perdew <thomasprdw@aol.com>
2018-09-17 17:40:14 -0600

    Added text to stats

--- Stats/Strength.php
+++ Stats/Strength.php
@@ -29,20 +29,33 @@
       <li><a href="Stats/Wisdom.html">Halfling</a></li>
     </ul>
     <li>Classes</li>
-		<ul class="content">
-      <li><a href="Stats/Strength.php"></a>Fighter</li>
-      <li><a href="Stats/Dexterity.html">Cleric</a></li>
-      <li><a href="Stats/Constitution.html">Wizard</a></li>
-      <li><a href="Stats/Intelligence.html">Rogue</a></li>
-    </ul>
+                <ul class="content">
+                    <li><a href="Classes/Fighter.html"></a>Fighter</li>
+                    <li><a href="Classes/Cleric.html">Cleric</a></li>
+                    <li><a href="Classes/Wizard.html">Wizard</a></li>
+                    <li><a href="Classes/Rogue.html">Rogue</a></li>
+                </ul>
     <li>Alignment</li>
     <li>Money</li>
     <li>Equipment</li>
     <li>Height and Weight</li>
   </div>
 </div>
-        <div class="item3">Strength</div>
-
+        <div class="item3"><h2>Strength</h2>
+		<p>Strength (“Str”) is a measure of physical power, and is the most
+important attribute for fighters. If a fighter, paladin, or ranger
+character rolls an 18 strength, the player then rolls a d%, and
+that number is added as a decimal, or percentage, to the 18
+strength. (See the table below for details.) A roll of 00 on the
+percentage die indicates a strength score of 19. Members of
+other classes cannot naturally gain strength in excess of 18.</p>
+<p>The following table summarises the bonuses and penalties for
+strength scores. Note that an “Extraordinary Success” indicates
+the possibility for extremely strong characters to perform
+exceptional or normally impossible feats of strength, such as
+opening a door which is held closed by some relatively minor
+magic rather than a lock.</p>
+		</div>
         <div class="item5">Footer</div>
     </div>
 
