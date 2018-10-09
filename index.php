348c628306b0928a4796b7c2f7aaee50cbd0083f
thomas perdew <thomasprdw@aol.com>
2018-10-08 19:06:40 -0600

    more fixes

--- index.php
+++ index.php
@@ -3,18 +3,18 @@
 <html>
 <head>
   <title>OSRwiki</title>
-  <link rel="stylesheet" type="text/css" href="/stylesheet.css">
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
   <link rel="icon" href="favicon.ico">
 </head>
 <body>
     <div class="grid-container">
-        <div class="item1"><?php include '/Layout/Header.php';?></div>
+        <div class="item1"><?php include 'Layout/Header.php';?></div>
         <div class="item2">
-            <?php include '/Layout/Menu.php';?>
+            <?php include 'Layout/Menu.php';?>
         </div>
 		<div class="item3">Welcome to Osric Wiki. Please select an item from the menu on the left</div>
         <div class="item5">
-            <?php include '/Layout/Footer.php';?>
+            <?php include 'Layout/Footer.php';?>
         </div>
     </div>
 
