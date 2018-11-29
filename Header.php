d7c932d37822c141da03c91746a8769f29222062
thomas perdew <thomasprdw@aol.com>
2018-11-28 18:26:12 -0700

    fixed links

--- Header.php
+++ Header.php
@@ -2,7 +2,7 @@
 session_start();
 ?>
 
- <img src="icon.png" class="icon-item"></img><h1 style="float:left; margin:0;"><a href="Index.php">OSRwiki</a></h1>
+ <img src="icon.png" class="icon-item"></img><h1 style="float:left; margin:0;"><a href="index.php">OSRwiki</a></h1>
 
  <?php
 if (!isset($_SESSION['logged_in']) || !$_SESSION['logged_in']) {?>
@@ -11,7 +11,7 @@ if (!isset($_SESSION['logged_in']) || !$_SESSION['logged_in']) {?>
 }
 else {
 	?>
-	<a href="Logout.php" class="login-item">Logout</a>
+	<a href="logout.php" class="login-item">Logout</a>
 	<?php
 }
 ?>
